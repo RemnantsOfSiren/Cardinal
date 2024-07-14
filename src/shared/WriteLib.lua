7aad34b5fcc2f42cf47d858173c53d66deef452b
Byran <bemery2@missouriwestern.edu>
2024-07-13 20:29:18 -0500

    patches

--- src/shared/WriteLib.lua
+++ src/shared/WriteLib.lua
@@ -1,7 +1,10 @@
 local WriteLib = {
     Give = function(Replica, Item: string, ItemData: {[string]: any} )
         Replica:Set(Item, ItemData)
-    end
+    end,
+    Take = function(Replica, Item: string)
+        Replica:Set(Item, nil)
+    end,
 }
 
 return WriteLib
