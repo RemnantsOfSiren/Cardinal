a2bd0716df1ba8e97f710af39752d0e19cccb618
Byran <bemery2@missouriwestern.edu>
2024-10-07 10:11:00 -0500

    test fixes

--- src/shared/WriteLib.lua
+++ src/shared/WriteLib.lua
@@ -1,10 +1,10 @@
 local WriteLib = {
-    Give = function(Replica, Item: string, ItemData: {[string]: any} )
-        Replica:Set(Item, ItemData)
-    end,
-    Take = function(Replica, Item: string)
-        Replica:Set(Item, nil)
-    end,
+	Give = function(Replica, Item: string, ItemData: { [string]: any })
+		Replica:Set(Item, ItemData)
+	end,
+	Take = function(Replica, Item: string)
+		Replica:Set(Item, nil)
+	end,
 }
 
 return WriteLib
